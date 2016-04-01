e6189c76ad8ddd5c322eb15325d7d05a40aad4c0
contemplat0r <uldo.forme@gmail.com>
2016-04-01 20:43:54 +0300

    ticket:1 begin write tests

--- apps/mission/tests.py
+++ apps/mission/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from mission.models import PersonDescription
 
 # Create your tests here.
+
+class PersonDescriptionTestCase(TestCase):
+    def setUp(self):
+        PersonDescription.objects.create(name='Leonarg', surname='Hofstadter', birthdate='01-10-1981', email='not.sheldon@gmail.com', phone='111111111')
