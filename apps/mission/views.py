f563c60d61ca526a3af508c3e53b20363889b3b1
contemplat0r <uldo.forme@gmail.com>
2016-04-05 19:36:57 +0300

    ticket:1 "home" view get values for template from PersonDescription model

--- apps/mission/views.py
+++ apps/mission/views.py
@@ -1,19 +1,31 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render_to_response
+from apps.mission.models import PersonDescription
 
 # Create your views here.
 
 
 def home(request):
-    context = {'person_description': {
-        'name': 'Nikolai',
-        'second_name': 'Golikov',
-        'birthdate': '1969-10-04',
-        'biography': ' Very, very long biography\n' +
-        '.' * 19 + '\n' + '.' * 19 + '\n' + 'Biography end.',
-        'email': 'uldo.forme@gmail.com',
-        'jabber_id': 'contemplat0r@42cc.co',
-        'skype_id': 'contemplat0r',
-        'other_contacts': 'Phone: +380507777777'}}
+    context = {'person_description': None}
+    try:
+        person_description = PersonDescription.objects.get(
+            email='uldo.forme@gmail.com'
+        )
+    except PersonDescription.DoesNotExist:
+        person_description = None
+
+    if person_description is not None:
+        context = {
+            'person_description': {
+                'name': person_description.name,
+                'second_name': person_description.surname,
+                'birthdate': person_description.birthdate,
+                'biography': person_description.bio,
+                'email': person_description.email,
+                'jabber_id': person_description.jabber,
+                'skype_id': person_description.skype,
+                'other_contacts': person_description.other_contacts
+            }
+        }
     return render_to_response('person.html', context)
