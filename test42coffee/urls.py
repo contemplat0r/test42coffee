d8cd2197d797d2179e52f536dd45d766f5afbac5
contemplat0r <uldo.forme@gmail.com>
2016-04-06 14:43:13 +0300

    ticket:1 temporary managing static content by django.views.static.serve view (by adding corresponding line in urls.py

--- test42coffee/urls.py
+++ test42coffee/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 from apps.mission.views import home
 admin.autodiscover()
@@ -12,4 +13,7 @@ urlpatterns = patterns(
 
     url(r'^$', home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
+
+
